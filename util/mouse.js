3ae3e8f8404445b755cc7168fea7efe9649d3482
Mangala SSS Khalsa <mkhalsa@sitepen.com>
2014-06-19 14:27:19 -0700

    Update mouse/util to take advantage of Dojo 1.8 updated dojo/mouse functionality

--- util/mouse.js
+++ util/mouse.js
@@ -1,35 +1,16 @@
-define(["dojo/on", "dojo/dom", "dojo/query"], function(on, dom){
-	function handler(selector, type){
-		// summary:
-		//		Creates a handler function usable as a simulated event to dojo/on,
-		//		which fires only if the mouse is moving into or out of the node of
-		//		interest indicated by the selector.
-		//		This is similar, but not identical, to what dojo/mouse does.
-		// selector: String
-		//		Query selector for event delegation.
-		// type: String
-		//		Event to delegate on (mouseover or mouseout).
-		
-		return function(node, listener){
-			return on(node, selector + ":" + type, function(evt){
-				if(!dom.isDescendant(evt.relatedTarget, this)){
-					return listener.call(this, evt);
-				}
-			});
-		};
-	}
-	
+define([
+	"dojo/mouse",
+	"dojo/on",
+	"dojo/query"
+], function (mouse, on) {
 	return {
 		// Provide enter/leave events for rows, cells, and header cells.
 		// (Header row is trivial since there's only one.)
-		enterRow: handler(".dgrid-content .dgrid-row", "mouseover"),
-		enterCell: handler(".dgrid-content .dgrid-cell", "mouseover"),
-		enterHeaderCell: handler(".dgrid-header .dgrid-cell", "mouseover"),
-		leaveRow: handler(".dgrid-content .dgrid-row", "mouseout"),
-		leaveCell: handler(".dgrid-content .dgrid-cell", "mouseout"),
-		leaveHeaderCell: handler(".dgrid-header .dgrid-cell", "mouseout"),
-		
-		// Also expose the handler function, so people can do what they want.
-		createDelegatingHandler: handler
+		enterRow: on.selector(".dgrid-content .dgrid-row", mouse.enter),
+		enterCell: on.selector(".dgrid-content .dgrid-cell", mouse.enter),
+		enterHeaderCell: on.selector(".dgrid-header .dgrid-cell", mouse.enter),
+		leaveRow: on.selector(".dgrid-content .dgrid-row", mouse.leave),
+		leaveCell: on.selector(".dgrid-content .dgrid-cell", mouse.leave),
+		leaveHeaderCell: on.selector(".dgrid-header .dgrid-cell", mouse.leave)
 	};
 });
