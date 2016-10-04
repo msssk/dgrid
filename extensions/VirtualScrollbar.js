ad42bb0de15a978e9c26ee02b0f291344c5b8eb4
Mangala SSS Khalsa <mkhalsa@sitepen.com>
2016-10-04 16:01:22 -0700

    VirtualScrollbar: WIP scaling preload nodes and calculating scroll factor

--- extensions/VirtualScrollbar.js
+++ extensions/VirtualScrollbar.js
@@ -4,6 +4,10 @@ define([
 	'dojo/on'
 ], function(declare, domConstruct, on) {
 	return declare(null, {
+		_scrollScaleFactor: 1,
+		_scrollContentMaxHeight: 10000,
+		_virtualScrollbarMaxHeight: 10000,
+
 		buildRendering: function() {
 			this.inherited(arguments);
 
@@ -49,9 +53,63 @@ define([
 		},
 
 		refresh: function() {
-			this.inherited(arguments);
+			var self = this;
 
-			this.scrollbarNode.firstChild.style.height = this.contentNode.scrollHeight + 'px';
+			return this.inherited(arguments).then(function(results) {
+				var contentHeight = self._calculateContentHeight();
+				var clampedHeight = Math.min(self._virtualScrollbarMaxHeight, contentHeight);
+
+				if (contentHeight > self._virtualScrollbarMaxHeight) {
+					self._scrollScaleFactor = contentHeight / clampedHeight;
+				}
+
+				self.scrollbarNode.firstChild.style.height = clampedHeight + 'px';
+
+				return results;
+			});
+		},
+
+		_calculateContentHeight: function() {
+			// grid._total: total number of currently rendered rows
+			// preload.count: number of rows the preload is holding space for
+			// preload.rowHeight: average height of sibling rows
+
+			var topPreload = this.preload;
+			var bottomPreload = topPreload.next;
+
+			return (topPreload.virtualHeight || topPreload.node.offsetHeight) +
+				(this._total * topPreload.rowHeight) +
+				(bottomPreload.virtualHeight || bottomPreload.node.offsetHeight);
+		},
+
+		_calculateScrollTop: function() {
+			return this.scrollbarNode.scrollTop * this._scrollScaleFactor;
+		},
+
+		getScrollPosition: function() {
+			return {
+				x: this.bodyNode.scrollLeft,
+				y: this._calculateScrollTop()
+			};
+		},
+
+		_adjustPreloadHeight: function(preload, noMax) {
+			var height = this._calculatePreloadHeight(preload, noMax);
+			var clampedHeight = Math.min(this._scrollContentMaxHeight - this.bodyNode.scrollHeight + preload.node.offsetHeight, height);
+
+			if (clampedHeight === height) {
+				preload.clampedHeight = 0;
+				preload.virtualHeight = 0;
+			} else {
+				preload.clampedHeight = clampedHeight;
+				preload.virtualHeight = height;
+			}
+
+			if (preload.next) {
+				preload.node.style.height = clampedHeight + 'px';
+			} else {
+				preload.node.style.height = '1000px';
+			}
 		}
 	});
 });
